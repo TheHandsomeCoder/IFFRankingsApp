e84b23dfd6c8d0024a87ec1dec1b438c6a54e117
Scott O'Malley <somalley@guidewire.com>
2015-05-29 18:15:20 +0100

    Sync

--- client/assets/js/controllers/CompetitionController.js
+++ client/assets/js/controllers/CompetitionController.js
@@ -1,18 +1,20 @@
 angular.module('application')
     .controller('CompetitionController', CompetitionController);
 
-CompetitionController.$inject = ['$scope', '$stateParams', '$state', '$controller', 'CompetitionService'];
-function CompetitionController($scope, $stateParams, $state, $controller, CompetitionService)
+CompetitionController.$inject = ['$scope', '$stateParams', '$state', '$controller', 'CompetitionService', 'FencerService'];
+function CompetitionController($scope, $stateParams, $state, $controller, CompetitionService, FencerService)
 {
     angular.extend(this, $controller('DefaultController', {$scope: $scope, $stateParams: $stateParams, $state: $state}));
     
     $scope.competitions = CompetitionService.competitions;
+    $scope.fencers = FencerService.fencers;
+
     var competitionID = $stateParams.id;
     $scope.competition = CompetitionService.getCompetition(competitionID);
     $scope.results = [];
 
     $scope.addNewResult = function(){
-    	$scope.results.push({rank:1, fencer:"Scott O'Malley", points: 55});
+    	$scope.results.push({rank:1, fencer:{name:"Scott O'Malley",club:"Phoenix"}, points: 55});
     };
 
 }
