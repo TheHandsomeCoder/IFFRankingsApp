94c15dddc20487cbcc5f8fab53aff4a8ed740b05
Scott O'Malley <somalley@guidewire.com>
2015-06-03 17:38:46 +0100

    Added back in SCSS location

--- gulpfile.js
+++ gulpfile.js
@@ -24,7 +24,8 @@ var paths = {
   sass: [
     'client/assets/scss',
     'bower_components/foundation/scss/foundation/components',      
-    'bower_components/allmighty-autocomplete/css',      
+    'bower_components/foundation-apps/scss',
+    'bower_components/allmighty-autocomplete/css',
      require('node-bourbon').includePaths
 
   ],
